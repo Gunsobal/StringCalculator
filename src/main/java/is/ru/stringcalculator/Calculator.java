1b8a4de783fa6933d696feeed9d311cd050fd00a
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-10-07 11:29:11 +0000

    Finished cycle for testing one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,7 @@ package is.ru.stringcalculator;
 public class Calculator
 {
     public static int add(String text){
-        return 0;
+        if (text == "") return 0;
+        return 1;
     }
 }
