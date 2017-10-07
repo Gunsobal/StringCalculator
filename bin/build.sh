0e2f430962e16fc52207f93e12f8259c8baed1df
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-10-07 11:21:53 +0000

    Initial setup of project

---
+++ bin/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./bin/clean.sh
+./bin/compile.sh
+./bin/compile_tests.sh
+./bin/test.sh
---
+++ bin/compile_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp "src/test/resources/junit-4.12.jar;classes/" src/test/java/is/ru/stringcalculator/*.java -d classes
